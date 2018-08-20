417068e94db8c515c21bb90e1fe3a8905cdfef39
shamoyuu <shamoyuu@qq.com>
2018-08-20 12:03:13 +0800

    矩形绘制修改拖动删除完成

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # geojson-image
 不再是地图，而是对一张图片设置热区，生成geojson数据
+
+## 在线预览
+[点击预览](http://60230.cn/canvas/index.html)
---
+++ js/rect.js
@@ -0,0 +1,25 @@
+function Rect() {
+	createjs.Shape.call(this);
+	
+	this.shapeId = Date.now()
+
+	this.paintDashed = function(x1, y1, x2, y2) {
+		this.graphics = new createjs.Graphics();
+		this.graphics.setStrokeDash([4, 5]).beginStroke("#f00")
+			.beginFill("rgba(255, 128, 0, 0.1)")
+			.drawRect(0, 0, x2 - x1, y2 - y1)
+		this.x = x1;
+		this.y = y1;
+	}
+	
+	this.paint = function(x1, y1, x2, y2) {
+		this.graphics = new createjs.Graphics();
+		this.graphics.clear().beginStroke("#f00")
+        		.beginFill("rgba(255, 128, 0, 0.1)")
+        		.drawRect(0, 0, x2 - x1, y2 - y1);
+        this.x = x1;
+		this.y = y1;
+	}
+}
+
+Rect.prototype = new createjs.Shape();
